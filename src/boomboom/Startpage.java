9c8fd5343f90d98315b72a025bcb852d9ef840d0
yehia67 <yehiatarek67@gmail.com>
2017-04-24 22:00:21 +0200

    begin the start page and finish the layout

--- src/boomboom/Startpage.java
+++ src/boomboom/Startpage.java
@@ -4,11 +4,61 @@
  * and open the template in the editor.
  */
 package boomboom;
-
-/**
- *
- * @author Sony
- */
-public class startPage {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Startpage extends JFrame {
+	  GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+ int WIDTH = gd.getDisplayMode().getWidth();
+int HEIGHT = gd.getDisplayMode().getHeight();
+      private JLabel startt;	
+       private JLabel optionn;
+	   private JLabel helpp;
+	   private JLabel exitt;  
+       
+       
+       private JButton start;
+	   private JButton MULTIPLIER;
+	   private JButton dashboard;
+	   private JButton exit;
+	   private String title;
+	   private ImageIcon iq;
+	   
+	   
+	   
+	   public Startpage(){
+		   setLayout(null);
+		   start = new JButton("Start");
+		   start.setBounds(WIDTH/2 + WIDTH/50 - WIDTH/10, HEIGHT/6, 200, 100);
+                   add(start);
+		   MULTIPLIER = new JButton("MULTIPALIER");
+		   MULTIPLIER.setBounds(WIDTH/2 + WIDTH/50 - WIDTH/10,HEIGHT/3  , 200, 100);
+                   add(MULTIPLIER);
+		   optionn = new JLabel("");
+		   dashboard = new JButton("DashBoard");
+                   dashboard.setBounds(WIDTH/2 + WIDTH/50 - WIDTH/10,HEIGHT/2  , 200, 100);
+		   add(dashboard);
+		   exit = new JButton("Exit");
+                   exit.setBounds(WIDTH/2 + WIDTH/50 - WIDTH/10,HEIGHT-HEIGHT/3  , 200, 100);
+			add(exit);
+			//iq = new ImageIcon(getClass().getResource("iq.jpg"));
+	   
+	   }
+	   
+	   
+	
+	public static void main(String args[])
+{      
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+       int WIDTH = gd.getDisplayMode().getWidth();
+int HEIGHT = gd.getDisplayMode().getHeight();
+	Startpage x = new Startpage();
+	x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        x.setLocationRelativeTo(null);
+	x.setVisible(true);
+	x.setSize(WIDTH,HEIGHT);
+	x.setResizable(false);
+	x.setTitle("BOOMBOOM!!");
+	//x.pack();
+}
 }
